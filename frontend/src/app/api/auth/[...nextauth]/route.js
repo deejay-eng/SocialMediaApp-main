65f8d15685095e54bd518ff754d62e1fd1fe8952
D_J <148381865+deejay-eng@users.noreply.github.com>
2025-11-18 22:27:01 +0530

    Update route.js

--- frontend/src/app/api/auth/[...nextauth]/route.js
+++ frontend/src/app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-console.log('NEXTAUTH_SECRET:', process.env.NEXTAUTH_SECRET);
+
 
 export const authOptions = {
 	providers: [
