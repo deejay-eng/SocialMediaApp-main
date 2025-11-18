9f580d3d71b49a9d90b3bc27dd31e0271ff4a185
D_J <148381865+deejay-eng@users.noreply.github.com>
2025-11-19 01:17:35 +0530

    Update user.controllers.js

--- backend/src/controllers/user.controllers.js
+++ backend/src/controllers/user.controllers.js
@@ -284,6 +284,9 @@ export const updateUser = asyncHandler(async (req, res, next) => {
 
 // Update only profile picture via Cloudinary upload or URL
 export const updateUserAvatar = asyncHandler(async (req, res, next) => {
+  console.log('DEBUG req.file:', req.file);
+  console.log('DEBUG req.body:', req.body);
+
   const userId = req.user.id;
   const user = await User.findById(userId);
   if (!user) {
