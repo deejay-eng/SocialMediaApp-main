c6490e869964c4d063c6ecc1fa90930f67a772e2
D_J <148381865+deejay-eng@users.noreply.github.com>
2025-11-19 15:09:01 +0530

    Update ProfilePicVerify.js

--- frontend/src/components/userProfile/ProfilePicVerify.js
+++ frontend/src/components/userProfile/ProfilePicVerify.js
@@ -54,12 +54,12 @@ const ProfilePicVerify = () => {
         const uploadRes = await axios.put(
           `${process.env.NEXT_PUBLIC_BACKEND_API}/api/user/update/avatar`,
           formData,
-          { headers: { 'auth-token': authToken } }
+          { headers: { 'auth-token': authToken, "Content-Type": "multipart/form-data" } }
         );
         const verifyRes = await axios.put(
           `${process.env.NEXT_PUBLIC_BACKEND_API}/api/user/update`,
           { isDpVerify: true },
-          { headers: { 'auth-token': authToken } }
+          { headers: { 'auth-token': authToken, "Content-Type": "multipart/form-data" } }
         );
         dispatch(updateUserDetails(verifyRes.data.data || uploadRes.data.data));
       } else {
@@ -68,7 +68,7 @@ const ProfilePicVerify = () => {
           { profilePicture: currentPic, isDpVerify: true },
           {
             headers: {
-              "auth-token": authToken,
+              "auth-token": authToken, "Content-Type": "multipart/form-data"
             },
           }
         );
